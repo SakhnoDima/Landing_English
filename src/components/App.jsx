df2ac6a4b6339dc7a206ae118183ae1e9df83578
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-11-09 12:36:17 +0200

    Footer
    Footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import ClientSay from './ClientSay/ClientSay';
 import Count from './Count/Count';
+import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Hero from './Hero/Hero';
 import WhyMe from './WhyMe/WhyMe';
@@ -12,6 +13,7 @@ export const App = () => {
       <Count />
       <WhyMe />
       <ClientSay />
+      <Footer />
     </>
   );
 };
